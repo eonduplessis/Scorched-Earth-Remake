584a5cfc9a661492502951b63a1fad5310c81b2a
Eon du Plessis <mess9991+git@gmail.com>
2011-12-04 17:31:31 +0200

    DirectX Added
    DirectX testing

---
+++ Scorched Earth Remake/Scorched Earth Remake/Test.cs
@@ -0,0 +1,14 @@
+﻿using System;
+//using System.Collections.Generic;
+using System.Linq;
+//using System.Text;
+
+using Microsoft.DirectX;
+using Microsoft.DirectX.Direct3D;
+
+namespace Scorched_Earth_Remake
+{
+    class Test
+    {
+    }
+}
